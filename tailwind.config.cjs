e10105424944febd4dd3ef9a1b7f5d2d9708fc32
UrielMartinez21 <uriellmartinezz35@gmail.com>
2022-12-04 17:01:03 -0600

    Instalacion de tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //--> Archivos donde aplicara las clases
+  content: [                      
+    './index.html',     
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
