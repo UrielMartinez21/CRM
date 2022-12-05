bb615f43c8ba8d91e4ea2e6aaf6525c776077c49
UrielMartinez21 <uriellmartinezz35@gmail.com>
2022-12-04 21:09:14 -0600

    Instalacion de React-Router-DOM

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <h1 className='text-6xl font-bold'>Router DOM</h1>
-  )
-}
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,                  // Definir rutas por medio de objeto principal
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([    // Arreglo de rutas
+  {
+    path: '/',                          // './'  para pagina principal
+    element:(<h1>Desde inicio</h1>)     // Lo que se mostrara en la pagina
+  },
+  {
+    path: '/test',
+    element:(<h1>Desde test</h1>)
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider
+      router={router}
+    />
   </React.StrictMode>
 )
